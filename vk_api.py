12a10f62fb31764b62f2bce7429d2e30215c34af
Myorthy <46349046+Myorthy@users.noreply.github.com>
2019-02-16 13:02:46 +0500

    Update vk_api.py

--- vk_api.py
+++ vk_api.py
@@ -1,5 +1,7 @@
+import requests
 from urllib.parse import urlencode
-APP_ID = 0000000
+
+APP_ID = 6862932
 AUTH_URL = 'https://oauth.vk.com/authorize'
 auth_data = {
     'client_id': APP_ID,
@@ -8,12 +10,42 @@ auth_data = {
     'response_type': 'token',
     'v': '5.92',
 }
-TOKEN = 'acce4e3112156fc596406ce'
-params = {
-'v': '5.92'
-}
-response = requests.get('https://api.vk.com/method/status.get', params)
+TOKEN = 'acce4e3112156fc596406cec7c3c201ce0d4da214bc5ea6a82eb0e2ab9cdc1a3e4059ef1d97441dc71b88'
+
+# print('?'.join((AUTH_URL, urlencode(auth_data))))
+
+class User:
+
+    def __init__(self, token):
+        self.token = token
+
+    def get_params(self):
+        return {
+        'v': '5.92',
+        'access_token': TOKEN,
+        }
+
+    def get_status(self):
+        params = self.get_params()
+        response = requests.get('https://api.vk.com/method/status.get', params)
+        return response.json()
+
+    def set_status(self, text):
+        params = self.get_params()
+        params['text'] = text
+        response = requests.get('https://api.vk.com/method/status.set', params)
+        return response.json()
+
+    def mutual_friends(self, user_id):
+        params = self.get_params()
+        params['target_uid'] = user_id
+        response = requests.get('https://api.vk.com/method/friends.getMutual', params)
+        return response.json()
 
 
 
-print('?'.join((AUTH_URL, urlencode(auth_data))))
+
+
+user = User(TOKEN)
+print(user.get_status())
+print(user.mutual_friends(90826116))
